255ad9d331fde2fe96639fae0767a4aa76d6106c
luisvilca10 <martinvilca@upeu.edu.pe>
2024-09-03 06:28:16 -0500

    Mejoras UX

--- src/App.jsx
+++ src/App.jsx
@@ -148,11 +148,11 @@ function App() {
         <div className="container mx-auto">
           <div className="mx-8 lg:mx-3 flex flex-col gap-y-4 lg:flex-row items-center justify-between py-2 text-center ">
             <div className="flex gap-x-3 flex-col items-center">
-              <img
+              {/* <img
                 className="h-10 w-1/2"
                 src="../src/assets/logo.png"
                 alt=""
-              />
+              /> */}
               <div className=" font-serif text-white ">
                 30 de agosto del 2024
               </div>
